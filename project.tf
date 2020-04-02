f665f4ba5062c793e964a5db56b7153f4d5684a0
Ransom Maeda <44108151+ransom-maeda@users.noreply.github.com>
2020-04-02 12:36:33 -0700

    Add files via upload

---
+++ project.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  region = var.region
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+  prefix      = var.project_name
+}
+
+resource "google_project" "project" {
+  name            = var.project_name
+  project_id      = random_id.id.hex
+  billing_account = var.billing_account
+  org_id          = var.org_id
+}
