8e13f41410b8a5830da9cfbee6108cfe26bef3ba
Ransom Maeda <random.maeda@gmail.com>
2020-04-05 13:45:05 -0700

    add .gitignore and variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+  type = string
+}
+
+variable "billing_account" {
+  type = string
+}
+
+variable "org_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
